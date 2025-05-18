28934316b9a57d0da7cf01fffc15b707fb9e1bf5
Ethereal55 <davidbaymanov@gmail.com>
2025-05-18 19:20:07 +0300

    library
    Удалил директорию, для уменьшения размера репозитория (Она создается сама при запуске проекта на unity)

--- Project/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/2f1m61e3/logs/arm64-v8a/build_command_game.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Unity\\6000.0.40f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Unity\\Projects\\TRPP_Mobile\\.utmp\\RelWithDebInfo\\2f1m61e3\\arm64-v8a" ^
-  game
